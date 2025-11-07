f44ecf4128b795f9fa870b8621f376e9fbc57be1
Nikhil <nikhilgelli1@gmail.com>
2025-11-07 12:16:13 +0530

    added cAdvisor

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,6 +24,13 @@ resource "aws_security_group" "ec2-sg" {
     }
 
     ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
         from_port = 22
         to_port = 22
         protocol = "tcp"

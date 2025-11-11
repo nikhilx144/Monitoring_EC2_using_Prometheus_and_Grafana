03ca7eae209e90b0a2c453f96689f9e507126600
Nikhil <nikhilgelli1@gmail.com>
2025-11-11 22:26:16 +0530

    modified index.html

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,14 +6,14 @@ resource "aws_security_group" "ec2-sg" {
         from_port = 9090
         to_port = 9090
         protocol = "tcp"
-        cidr_blocks = ["${var.my_public_ip}/32"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     ingress {
         from_port = 9100
         to_port = 9100
         protocol = "tcp"
-        cidr_blocks = ["${var.my_public_ip}/32"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     ingress {
@@ -27,21 +27,21 @@ resource "aws_security_group" "ec2-sg" {
         from_port = 3000
         to_port = 3000
         protocol = "tcp"
-        cidr_blocks = ["${var.my_public_ip}/32"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     ingress {
         from_port = 8080
         to_port = 8080
         protocol = "tcp"
-        cidr_blocks = ["${var.my_public_ip}/32"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     ingress {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = ["${var.my_public_ip}/32"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     egress {
